7a5e786535080b47757114abb4ea05a83e9c1135
Daimu10 <idaira.princesita@gmail.com>
2025-10-17 20:38:46 -0500

    Añadir main

--- Actividad2/src/main/java/com/figuras/actividad2/Main.java
+++ Actividad2/src/main/java/com/figuras/actividad2/Main.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.figuras.actividad2;
 
-/**
- *
- * @author Usuario
- */
+
+import com.figuras.actividad2.Controller.Controller;
+import Model.Competencia;
+import View.View;
+
 public class Main {
-    
+	public static void main(String[] args) {
+		// Inicializar MVC
+		Competencia competencia = new Competencia("Mundial Pista - Cali");
+		Controller controller = new Controller(competencia);
+		View view = new View();
+		// Delegar interacción con el usuario a la vista
+		view.iniciarMenu(controller);
+	}
 }
